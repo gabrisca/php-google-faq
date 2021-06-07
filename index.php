a89c172c00b36092b67497d316ebb09e8c4b5536
gabrisca <gabrielescarparo1985@gmail.com>
2021-06-07 15:57:36 +0200

    html e css bozza ok

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <section id="top">
+      <div class="logo">google</div>
+      <div class="account">account</div>
+    </section>
+    <section id="bottom">
+      <ul>
+        <li>
+          <a href="#">Introduzione</a>
+        </li>
+        <li>
+          <a href="#">Nome sulla privacy</a>
+        </li>
+        <li>
+          <a href="#">Termini di servizio</a>
+        </li>
+        <li>
+          <a href="#">Tecnologie</a>
+        </li>
+        <li>
+          <a href="#">Domande frequenti</a>
+        </li>
+      </ul>
+    </section>
+  </header>
+  <main>
+   
+  </main>
+  <footer>
+    <ul>
+      <li>
+        <a href="#">Google</a>
+      </li>
+      <li>
+        <a href="#">Tutto su Google</a>
+      </li>
+      <li>
+        <a href="#">Privacy</a>
+      </li>
+      <li>
+        <a href="#">Termini</a>
+      </li>
+    </ul>
+    <div>
+      Italiano
+    </div>
+  </footer>
+</body>
+
+</html>
