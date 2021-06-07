a4855b38d40723cb29bb1766605b844f3f1b0a0e
gabrisca <gabrielescarparo1985@gmail.com>
2021-06-07 16:48:31 +0200

    inserito <br> con nl2br

--- index.php
+++ index.php
@@ -64,6 +64,10 @@ Se il paese associato al tuo account non corrisponde al tuo paese di residenza, 
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet"> 
+  <!-- style.css -->
   <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
@@ -104,7 +108,7 @@ Se il paese associato al tuo account non corrisponde al tuo paese di residenza, 
           <?php echo $faq['question'] ?>
         </h2>
         <p>
-          <?php echo $faq['answer'] ?>
+          <?php echo nl2br($faq['answer']) ?>
         </p>
       </div>
       <?php } ?>
